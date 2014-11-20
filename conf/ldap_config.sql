1d83aa2d99bfbe7a35030f79c71175f7259be857
kx27 <kx27@free.fr>
2014-11-20 23:57:16 +0100

    Revert "master"
    This reverts commit 95e4a98c9f4288f949b24e1888a3eb796216f18e.

--- conf/ldap_config.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO config (`plugin`, `key`, `value`) VALUES 
-('core', 'ldap:basedn', 'ou=users,dc=yunohost,dc=org'),
-('core', 'ldap:domain', 'dc=yunohost,dc=org'),
-('core', 'ldap:enable', '1'),
-('core', 'ldap:filter', 'objectClass=mailAccount'),
-('core', 'ldap:host', 'localhost'),
-('core', 'ldap:port', '389'),
-('core', 'sso:allowRemoteUser', '1');
